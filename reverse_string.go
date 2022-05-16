7ef9aed71943ee482a9c0469389bf69214e5d561
Anchousfish <anchousfish@gmail.com>
2022-05-16 15:13:13 +0300

    ver. 2

--- reverse_string.go
+++ reverse_string.go
@@ -8,10 +8,12 @@ import (
 func ReverseString(input string) (output string) {
 
 	some_string, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	//line:=make([]rune,0,len(some_string))
-	for i := range some_string {
-		output += string(some_string[len(some_string)-1-i])
-	}
+	line := []rune(some_string)
+	line2 := make([]rune, len(some_string))
 
+	for i := range line {
+		line2[i] = line[len(some_string)-1-i]
+	}
+	output = string(line2)
 	return output
 }
