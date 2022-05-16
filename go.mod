6e020d3b18e398d31b87d9d151b6afc1c428300c
Anchousfish <anchousfish@gmail.com>
2022-05-16 15:02:31 +0300

    version 1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Anchousfish/golang-united-school-homework-4.1
+
+go 1.18
--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,17 @@
 package reverse_string
 
+import (
+	"bufio"
+	"os"
+)
+
 func ReverseString(input string) (output string) {
-	// solution goes here
+
+	some_string, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	//line:=make([]rune,0,len(some_string))
+	for i := range some_string {
+		output += string(some_string[len(some_string)-1-i])
+	}
+
 	return output
 }
