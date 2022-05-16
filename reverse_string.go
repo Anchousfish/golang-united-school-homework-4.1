c5dd5d6ba2f127cce0bcaa663dab94b209a465d6
Anchousfish <anchousfish@gmail.com>
2022-05-16 15:35:21 +0300

    ver 4

--- reverse_string.go
+++ reverse_string.go
@@ -1,20 +1,20 @@
 package reverse_string
 
 import (
-	"fmt"
-	//"bufio"
-	//"os"
+	//"fmt"
+	"bufio"
+	"os"
 )
 
 func ReverseString(input string) (output string) {
-	var some_string string
-	//some_string, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-	fmt.Scan(&some_string)
+	//var some_string string
+	some_string, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	//fmt.Scan(&some_string)
 	line := []rune(some_string)
-	line2 := make([]rune, len(some_string))
+	line2 := make([]rune, len(line))
 
 	for i := range line {
-		line2[i] = line[len(some_string)-1-i]
+		line2[i] = line[len(line)-1-i]
 	}
 	output = string(line2)
 	return output
